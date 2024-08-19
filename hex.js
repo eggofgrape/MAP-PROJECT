d5e9a802e84e0f95c149268c621b605a0300e693
eggofgrape <seobin0007@naver.com>
2024-08-19 19:29:33 +0900

    Update hex.js

--- hex.js
+++ hex.js
@@ -4,16 +4,15 @@ const btn=document.getElementById('btn')
 //const: cannot be reassigned, redeclared
 const color=document.querySelector(".color");
 
-btn.addEventListener('click',function(){
-let hexColor='#'; //need 6 loops for 6 colorcode. 
+btn.addEventListener("click",function(){
+let hexColor="#"; //need 6 loops for 6 colorcode. 
 for(let i=0;i<6;i++){
-    hexColor+=hex[getRandomNumber()];
+    hexColor += hex[getRandomNumber()];
 }
 color.textContent=hexColor;
 document.body.style.backgroundColor=hexColor;
-})
+});
 
 function getRandomNumber(){
-    return Math.floor(Maht.random()*hex.length);
+    return Math.floor(Math.random() *hex.length);
 }//times hex's array length
-
