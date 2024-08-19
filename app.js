c5a73ddc964cfb76d302454432bf89c0eb876f9f
eggofgrape <seobin0007@naver.com>
2024-08-19 19:29:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const colors=["green","red","rgba(133,122,200)",
     "#f15025"];
-    const btn=document.getElementById('btn');
+    const btn=document.getElementById("btn");
     const color=document.querySelector(".color");
 
-    btn.addEventListener('click',function(){
+    btn.addEventListener("click",function(){
         //get random number between 0-3
         const randomNumber=getRandomNumber();
         console.log(randomNumber);
         document.body.style.backgroundColor=colors[randomNumber];
-        color.textContent=colors[randonNumber];
+        color.textContent=colors[randomNumber];
 
 
     });
